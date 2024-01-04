0b7132adb4f2f29266902f525403b3f5e4cbf98d
kuro <toshras22@gmail.com>
2024-01-04 11:28:18 +0900

    メモ追加アプリを実装

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,5 +5,53 @@ export const App: FC = () => {
   const [text, setText] = useState<string>('')
   const [memos, setMemos] = useState<string[]>([])
 
-  return <h1>簡単メモアプリ</h1>
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
+    setText(e.target.value)
+
+  const onClickAdd = () => {
+    const newMemos = [...memos]
+    newMemos.push(text)
+    setMemos(newMemos)
+    setText('')
+  }
+
+  const onClickDelete = (index: number) => {
+    const newMemos = [...memos]
+    newMemos.splice(index, 1)
+    setMemos(newMemos)
+  }
+
+  return (
+    <div>
+      <h1>簡単メモアプリ</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>追加</SButton>
+      <SContainer>
+        <p>メモ一覧</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={memo}>
+              <SMemoWrapper>
+                <p>{memo}</p>
+                <SButton onClick={() => onClickDelete(index)}>削除</SButton>
+              </SMemoWrapper>
+            </li>
+          ))}
+        </ul>
+      </SContainer>
+    </div>
+  )
 }
+
+const SButton = styled.button`
+  margin-left: 16px;
+`
+
+const SContainer = styled.div`
+  border: solid 1px #ccc;
+`
+
+const SMemoWrapper = styled.div`
+  display: flex;
+  aligin-items: center;
+`
